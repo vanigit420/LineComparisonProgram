7b6d654a68267c912c7ea401f1f32a4c301be2fd
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-14 10:41:13 +0530

    calling the method

---
+++ LineComparisonProgram/LineComparisonProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisonProram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Computation Program");
+            CalculateLength.get();
+
+        }
+    }
+}
