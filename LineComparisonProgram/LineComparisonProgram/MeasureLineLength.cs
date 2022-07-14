b13d02084b9578671a35f811b61a10c2cc7d4b6f
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-14 10:56:19 +0530

    comparing two line

--- LineComparisonProgram/LineComparisonProgram/MeasureLineLength.cs
+++ LineComparisonProgram/LineComparisonProgram/MeasureLineLength.cs
@@ -25,17 +25,23 @@ namespace LineComputation
         {
             MeasureLineLength line1 = new MeasureLineLength(1);
             MeasureLineLength line2 = new MeasureLineLength(2);
-            double lengthofLine1 = Math.Sqrt(Math.Pow((line1.x2 - line1.x1), 2) + Math.Pow((line1.y2 - line1.y1), 2));
-            double lengthofLine2 = Math.Sqrt(Math.Pow((line2.x2 - line2.x1), 2) + Math.Pow((line2.y2 - line2.y1), 2));
+            double lengthofLine1 = Math.Round(Math.Sqrt(Math.Pow((line1.x2 - line1.x1), 2) + Math.Pow((line1.y2 - line1.y1), 2)), 2);
+            double lengthofLine2 = Math.Round(Math.Sqrt(Math.Pow((line2.x2 - line2.x1), 2) + Math.Pow((line2.y2 - line2.y1), 2)), 2);
 
             if (lengthofLine2 == lengthofLine1)
             {
                 Console.WriteLine("Both lines are Equal or Identical");
-
+                Console.WriteLine("Line1 : " + lengthofLine1 + " Line2 : " + lengthofLine2);
+            }
+            else if (lengthofLine1 > lengthofLine2)
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+                Console.WriteLine("Line1 : " + lengthofLine1 + " Line2 : " + lengthofLine2);
             }
             else
             {
-                Console.WriteLine("Both lines are not Identical");
+                Console.WriteLine("Line1 is lesser than Line2");
+                Console.WriteLine("Line1 : " + lengthofLine1 + " Line2 : " + lengthofLine2);
             }
         }
     }
